43671214b190bd4d93cf756fa7bbb0c66c8d72c4
BluGeek <92375729+blu-geek@users.noreply.github.com>
2026-05-14 16:44:26 -0600

    Update App.js with complete routing including auth pages and protected dashboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './styles/ThemeProvider';
 import GlobalStyles from './styles/GlobalStyles';
 import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import DashboardRoutes from './pages/DashboardRoutes';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
   return (
@@ -10,8 +14,19 @@ const App = () => {
       <GlobalStyles />
       <Router>
         <Routes>
+          {/* Public Routes */}
           <Route path="/" element={<LandingPage />} />
-          {/* Additional routes will be added as we develop more pages */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          
+          {/* Protected Dashboard Routes */}
+          <Route 
+            path="/dashboard/*" 
+            element={<ProtectedRoute element={<DashboardRoutes />} />} 
+          />
+          
+          {/* Catch-all redirect */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
     </ThemeProvider>
